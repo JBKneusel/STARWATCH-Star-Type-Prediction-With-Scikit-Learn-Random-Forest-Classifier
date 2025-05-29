da627e45d738a60c1afb74209ec1afd018cbdb69
Joseph K <josephkneusel@gmail.com>
2025-05-29 10:41:46 -0600

    edited data_wrangler
    Messing with values

--- data_wrangler.py
+++ data_wrangler.py
@@ -88,7 +88,7 @@ def train_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # Create and train the Random Forest model
-    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    clf = RandomForestClassifier(n_estimators=100, random_state=8675309)
     clf.fit(X_train, y_train)
 
     # Make predictions on the test set
